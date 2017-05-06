1ec90eeacb0b0f6e968ae50d2646cdf373fddfc9
root <root@70fb19f82387>
2017-05-06 15:31:18 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "autumn",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Alberstons_ChatBot_20170506",
+  "homePage" : "Main",
+  "name" : "Alberstons_ChatBot_20170506",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
